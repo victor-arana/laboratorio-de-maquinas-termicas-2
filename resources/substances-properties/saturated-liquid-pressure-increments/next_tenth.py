661e1a0a0e73614f9592bd3c0a43b26977a554c9
victor-arana <victor_arana@hotmail.com>
2021-02-06 06:30:30 -0600

    :refactor: read test cases from an external file
    Changes committed:
    	modified:   next_tenth.py
    	new file:   test-cases.csv

--- resources/substances-properties/saturated-liquid-pressure-increments/next_tenth.py
+++ resources/substances-properties/saturated-liquid-pressure-increments/next_tenth.py
@@ -1,3 +1,21 @@
+def readCSV(file_path, delim):
+    '''
+    Reads a csv file
+    file_path: path to the file
+    delim: delimiter used in the csv file
+    return: a list containing the read registers
+    '''
+    import csv
+    rows = []
+    with open(file_path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=delim)
+        for row in csv_reader:
+            col = []
+            for column in row:
+                col.append(float(column))
+            rows.append(col)
+    return rows
+
 def next_tenth(x):
     '''
     Given a real number x, return the closest integer
@@ -11,7 +29,7 @@ def next_tenth(x):
     if ( y % 10 ) == 0:
         return y
     elif (math.floor(y) % 10) == 0:
-        return math.floor(y)
+        return y
     else:
         y = math.floor(y)
         if (math.floor(y) % 10) == 0:
@@ -23,18 +41,7 @@ def next_tenth_test():
     '''
     test several cases for the function
     '''
-    test_cases = []
-    test_cases.append([-10.0, -10.0])
-    test_cases.append([0.0,0.0])
-    test_cases.append([10.0,10.0])
-    test_cases.append([0.5,0.0])
-    test_cases.append([5.0,10.0])
-    test_cases.append([9.6,10.0])
-    test_cases.append([-3.0,0.0])
-    test_cases.append([-30.0,-30.0])
-    test_cases.append([-36.5,-30.0])
-    test_cases.append([-1.0,0.0])
-
+    test_cases = readCSV('test-cases.csv',',')
     for t in test_cases:
         if next_tenth(t[0]) == t[1]:
             print('pass:', 'x:', t[0], 'actual:', next_tenth(t[0]), 'expected:', t[1])
