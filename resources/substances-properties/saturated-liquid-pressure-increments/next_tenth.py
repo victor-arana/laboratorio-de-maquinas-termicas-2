54eeecae48f5dd654eb486ccef70ef78460d6ea7
victor-arana <victor_arana@hotmail.com>
2021-02-06 05:31:36 -0600

    :refactor: specify decimal part for each test case
    Changes committed:
    	modified:   next_tenth.py

--- resources/substances-properties/saturated-liquid-pressure-increments/next_tenth.py
+++ resources/substances-properties/saturated-liquid-pressure-increments/next_tenth.py
@@ -1,11 +1,12 @@
 def next_tenth(x):
+    import math
     '''
     Given a real number x, return the closest integer
     number y such as x <= y, and b is divisible by 10.
     x: float number
     return y: closest integer such as x <= y and b % 10.
     '''
-    y = x + 10
+    y = math.floor(x) + 10
     return y
 
 def next_tenth_test():
@@ -13,14 +14,14 @@ def next_tenth_test():
     test several cases for the function
     '''
     test_cases = []
-    test_cases.append([0,10])
-    test_cases.append([0.5,0])
-    test_cases.append([5,10])
-    test_cases.append([9.6,10])
-    test_cases.append([-3,0])
-    test_cases.append([-30,-30])
-    test_cases.append([-36.5,-30])
-    test_cases.append([-1,0])
+    test_cases.append([0.0,10.0])
+    test_cases.append([0.5,0.0])
+    test_cases.append([5.0,10.0])
+    test_cases.append([9.6,10.0])
+    test_cases.append([-3.0,0.0])
+    test_cases.append([-30.0,-30.0])
+    test_cases.append([-36.5,-30.0])
+    test_cases.append([-1.0,0.0])
 
     for t in test_cases:
         if next_tenth(t[0]) == t[1]:
